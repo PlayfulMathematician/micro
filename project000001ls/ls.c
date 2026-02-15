bd137fd41826cef5bb681a4a70b1bab0dd17da62
Playful Mathematician <me@playfulmathematician.com>
2026-02-15 16:45:00 +0800

    i

--- project000001ls/ls.c
+++ project000001ls/ls.c
@@ -29,11 +29,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
-/*
-  stat(const char *restrict file, struct stat *restrict buf)
-  stat.st_gid
- *getgrgid(group id)
-*/
 
 enum LS_FLAGS {
   LS_a = 0x00000001,
@@ -41,6 +36,15 @@ enum LS_FLAGS {
   LS_A = 0x00000004,
   LS_A_or_a = 0x00000005
 };
+
+typedef struct {
+  char *filename;
+  char *permissions;
+  char* link_count;
+  char *owner;
+  char *owner_group;
+  char* file_size; // because -h
+} FileInfo;
 
 void print_file(const char *filename, char *path, uint32_t flags) {
   if (filename[0] == '.' && !(flags & LS_A_or_a)) {
@@ -58,9 +62,33 @@ void print_file(const char *filename, char *path, uint32_t flags) {
   strcpy(a_path, path);
   strcat(a_path, "/");
   strcat(a_path, filename);
-  char *real_path = realpath(a_path, NULL);
+  /// char *real_path = realpath(a_path, NULL);
   struct stat file_stat = {0};
-  stat(real_path, &file_stat);
+  lstat(a_path, &file_stat);
+  mode_t mod = file_stat.st_mode;
+  char permissions_string[12] = "rwxrwxrwx\0\0\0";
+  for (int permission = 0; permission < 9; permission++) {
+    if (!(mod & (1 << (8 - permission)))) {
+          permissions_string[permission] = '-';
+	}
+  }
+  /*
+     S_IFSOCK   0140000   socket
+           S_IFLNK    0120000   symbolic link
+           S_IFREG    0100000   regular file
+           S_IFBLK    0060000   block device
+           S_IFDIR    0040000   directory
+           S_IFCHR    0020000   character device
+           S_IFIFO    0010000   FIFO
+   */
+  char thing_type = ' ';
+  if (S_ISDIR(mod)) {
+    thing_type = 'd';
+  } else if (S_ISREG(mod)) {
+    thing_type = '-';
+  } else if (S_ISLNK(mod)) {
+    thing_type = 'l';
+  }
   // off_t      st_size;     /* Total size, in bytes */
   /*
     Plans for tommorow: Set up permissions, get permissions to work
@@ -88,7 +116,24 @@ void print_file(const char *filename, char *path, uint32_t flags) {
 
   char datetime_string[14];
   strftime(datetime_string, sizeof(datetime_string), "%b %d %H:%M", filetime);
-  printf("% *li %s %s % *li %s %s\n", 2, file_stat.st_nlink,
+  FileInfo fi = {};
+  fi.permissions = permissions_string;
+  // fi.link_count = file_stat.st_nlink; // resolve later
+  fi.owner = pass_struct->pw_name;
+  fi.owner_group = group_struct->gr_name;
+  // fi.file_size = file_stat.st_size;
+  // fi.data_time = datetime_string
+  fi.filename = (char*)filename;
+
+  // plans for tommorow:
+  // Make this export FileType thingy
+  // seperate filteration and this step
+  // iterate through files to get length
+  // store maybe in length data struct
+  // then we will have perfect alignment
+  // we can add more flags, easily
+  // goodbye :)
+  printf("%c%s % *li %s %s % *li %s %s\n",thing_type,permissions_string, 2, file_stat.st_nlink,
          pass_struct->pw_name, group_struct->gr_name, 6, file_stat.st_size,
          datetime_string, filename);
 }
